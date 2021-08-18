b533a153c2801bcd7bdb65eb925dafd767d3f369
Andrecast <acastrillonpuerta@gmail.com>
2021-08-19 01:51:08 +0700

    Puppet manifest to configuring nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,24 @@
 #Puppet manifest to install nginx
-package {'nginx':
+package { 'nginx':
   ensure   => 'latest',
   name     => 'nginx',
   provider => 'apt'
 }
+
 #Check if nginx service is running
-service {'nginx':
+service { 'nginx':
   ensure => running,
   enable => true
 }
+
 #Check redirection to youtube page
-exec {'redirec':
+exec { 'redirec':
   provider => 'shell',
   command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default'
 }
+
 #Check page info
-exec {'index':
+exec { 'index':
   provider => 'shell',
   command  => 'echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html'
 }
