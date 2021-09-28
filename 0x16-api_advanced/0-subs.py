cde0608520052f25e2840d7bb1eab434451c1365
Andrecast <acastrillonpuerta@gmail.com>
2021-09-28 10:39:03 +0700

    Comments

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+Prints number_of_subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """returns the number of subscribers for a given subreddit"""
     headers = {'User-Agent': 'Andre'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     res = requests.get(url, headers=headers, allow_redirects=False)
